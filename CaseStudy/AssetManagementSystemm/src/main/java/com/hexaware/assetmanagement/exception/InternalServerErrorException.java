b0061f1bcec012753a080c2d8353f6744c9c23a5
Rishabarajan C <115399042+rishabarajan@users.noreply.github.com>
2025-07-17 14:21:22 +0530

    Delete CaseStudy/AssetManagementSystemm directory

--- CaseStudy/AssetManagementSystemm/src/main/java/com/hexaware/assetmanagement/exception/InternalServerErrorException.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.assetmanagement.exception;
-
-
-public class InternalServerErrorException extends RuntimeException {
-
-	public InternalServerErrorException(String error) {
-		super(error);
-	}
-}
